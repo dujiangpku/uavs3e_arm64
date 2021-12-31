1ddd8ced65174ad6cbd2a3b6fc0e205cf7e8dc06
dujiangpku <85744777+dujiangpku@users.noreply.github.com>
2021-12-31 10:58:14 +0800

    整理代码

--- build/android/app/src/main/jni/uavs3e_arm64.mk
+++ build/android/app/src/main/jni/uavs3e_arm64.mk
@@ -1,17 +1,16 @@
 ARM64_SRC_PATH:= $(LOCAL_PATH)/src/armv8
 
-uavs3e_srcs_arm += $(ARM64_SRC_PATH)/arm64.c
-uavs3e_srcs_arm += $(ARM64_SRC_PATH)/itrans_arm64.c
 uavs3e_srcs_arm += $(ARM64_SRC_PATH)/def_arm64.S
-uavs3e_srcs_arm += $(ARM64_SRC_PATH)/pixel_arm64.S
-uavs3e_srcs_arm += $(ARM64_SRC_PATH)/itrans_dct2_arm64.S
-uavs3e_srcs_arm += $(ARM64_SRC_PATH)/itrans_dct8_dst7_arm64.S
+uavs3e_srcs_arm += $(ARM64_SRC_PATH)/arm64.c
 uavs3e_srcs_arm += $(ARM64_SRC_PATH)/inter_pred_arm64.S
-uavs3e_srcs_arm += $(ARM64_SRC_PATH)/deblock_arm64.S
-uavs3e_srcs_arm += $(ARM64_SRC_PATH)/alf_arm64.S
 uavs3e_srcs_arm += $(ARM64_SRC_PATH)/intra_pred_arm64.S
-uavs3e_srcs_arm += $(ARM64_SRC_PATH)/cost_arm64.S
-uavs3e_srcs_arm += $(ARM64_SRC_PATH)/transform_arm64.c
-uavs3e_srcs_arm += $(ARM64_SRC_PATH)/trans_dct2_arm64.S
+uavs3e_srcs_arm += $(ARM64_SRC_PATH)/trans_arm64.c
+uavs3e_srcs_arm += $(ARM64_SRC_PATH)/trans_dct_arm64.S
+uavs3e_srcs_arm += $(ARM64_SRC_PATH)/itrans_arm64.c
+uavs3e_srcs_arm += $(ARM64_SRC_PATH)/itrans_dct_arm64.S
+uavs3e_srcs_arm += $(ARM64_SRC_PATH)/deblock_arm64.S
 uavs3e_srcs_arm += $(ARM64_SRC_PATH)/sao_arm64.c
 uavs3e_srcs_arm += $(ARM64_SRC_PATH)/sao_kernel_arm64.S
+uavs3e_srcs_arm += $(ARM64_SRC_PATH)/alf_arm64.S
+uavs3e_srcs_arm += $(ARM64_SRC_PATH)/pixel_arm64.S
+uavs3e_srcs_arm += $(ARM64_SRC_PATH)/cost_arm64.S
